f2cb08baab5bd515b48d4affdf6aae276f884617
Yusung Sim <sys5867@gmail.com>
2018-11-13 22:56:46 +0100

    Add Maven quickstart directory

---
+++ p2p_stations/src/main/java/sys5867/ist_project/p2p_stations/App.java
@@ -0,0 +1,13 @@
+package sys5867.ist_project.p2p_stations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
